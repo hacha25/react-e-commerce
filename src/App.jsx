ec177e4ddeddd15a728f9121b1ad3a5c9ae6a111
Ayoub Hacha <hachaayoub888@gmail.com>
2025-11-26 00:20:38 +0100

    Fix import case for Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home"
 import Shop from "./pages/Shop"
 import Cart from "./pages/Cart"
 import Header from "./components/Header"
-import Footer from "./components/footer.jsx"
+import Footer from "./components/Footer"
 import ProductDetails from "./components/ProductDetails"
 import CartProvider from './context/CartContext.jsx'
 import { ToastContainer } from "react-toastify"
